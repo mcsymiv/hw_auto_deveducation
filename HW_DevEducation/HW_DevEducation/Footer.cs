ce6415c1a17390cc312c65710a4858fd006a06d2
mcsymiv <45435157+mcsymiv@users.noreply.github.com>
2020-09-29 10:49:01 +0300

    Add PO model to the main page + (header, footer)

---
+++ HW_DevEducation/HW_DevEducation/Footer.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_DevEducation
+{
+    public class Footer
+    {
+        By PolicyLink = By.XPath("/html/body/div[2]/footer/div/div[1]/a");
+        By FacebookLink = By.XPath("/html/body/div[2]/footer/div/ul/li[2]/a");
+        By InstaLink = By.XPath("/html/body/div[2]/footer/div/ul/li[3]/a");
+        By VkLink = By.XPath("/html/body/div[2]/footer/div/ul/li[4]/a");
+        By YoutubeLink = By.XPath("/html/body/div[2]/footer/div/ul/li[5]/a");
+        By LinkedLink = By.XPath("/html/body/div[2]/footer/div/ul/li[6]/a");
+        By TwitterLnk = By.XPath("/html/body/div[2]/footer/div/ul/li[7]/a");
+    }
+}
---
+++ HW_DevEducation/HW_DevEducation/Header.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_DevEducation
+{
+    public class Header
+    {
+        By LogoLink = By.XPath("/html/body/div[2]/div/header/div/a");
+        By CurrentLanguage = By.XPath("/html/body/div[2]/div/header/div/div[1]/ul/li");
+        By FloatButton = By.Name("entry");
+    }
+}
