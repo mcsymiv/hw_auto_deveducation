d69e89a750e4470d08fd53b97122bbfb3ce138fb
mcsymiv <45435157+mcsymiv@users.noreply.github.com>
2020-09-29 14:52:15 +0300

    Add UserOpensKyivCoursePage testCase

--- HW_DevEducation/HW_DevEducation/Test/DevedTest.cs
+++ HW_DevEducation/HW_DevEducation/Test/DevedTest.cs
@@ -21,25 +21,60 @@ namespace HW_DevEducation.Test
         [SetUp]
         public void OpenDevedPage()
         {
-
+            mp_POM = new MainPage(chrome);
             cp_POM = new CoursePage(chrome);
             head_city_ru_POM = new HeaderCityRu(chrome);
-            
             chrome.Navigate().GoToUrl("https://deveducation.com");
             chrome.Manage().Window.Maximize();
             chrome.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
         }
-        [TearDown]
+
+        [OneTimeTearDown]
         public void CloseBroser()
         {
             chrome.Quit();
         }
+
         [TestCase("Киев")]
+        [TestCase("Днепр")]
+        [TestCase("Харьков")]
+        [TestCase("Баку")]
+        [TestCase("Санкт-Петербург")]
         public void UserOpensKyivCoursePage(string localization)
         {
-            mp_POM.SelectCityOnMap(mp_POM.KyivLinkOnMap);
-            string localCityText = head_city_ru_POM.CurrentCityText(head_city_ru_POM.currentCity);
-            Assert.AreEqual(localization, localCityText);
+            string localCityText = string.Empty;
+            switch (localization)
+            {
+                case "Киев":
+                    mp_POM.SelectCityOnMap(mp_POM.KyivLinkOnMap);
+                    localCityText = head_city_ru_POM.CurrentCityText(head_city_ru_POM.currentCity);
+                    Assert.AreEqual(localization, localCityText);
+                    break;
+                case "Днепр":
+                    mp_POM.SelectCityOnMap(mp_POM.DniproLinkOnMap);
+                    localCityText = head_city_ru_POM.CurrentCityText(head_city_ru_POM.currentCity);
+                    Assert.AreEqual(localization, localCityText);
+                    break;
+                case "Харьков":
+                    mp_POM.SelectCityOnMap(mp_POM.KharkivLinkOnMap);
+                    localCityText = head_city_ru_POM.CurrentCityText(head_city_ru_POM.currentCity);
+                    Assert.AreEqual(localization, localCityText);
+                    break;
+                case "Баку":
+                    mp_POM.SelectCityOnMap(mp_POM.BakuLinkOnMap);
+                    localCityText = head_city_ru_POM.CurrentCityText(head_city_ru_POM.currentCity);
+                    Assert.AreEqual(localization, localCityText);
+                    break;
+                case "Санкт-Петербугр":
+                    mp_POM.SelectCityOnMap(mp_POM.SpbLinkOnMap);
+                    localCityText = head_city_ru_POM.CurrentCityText(head_city_ru_POM.currentCity);
+                    Assert.AreEqual(localization, localCityText);
+                    break;
+            }
+
         }
+
+
+
     }
 }
